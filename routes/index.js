65b2b45fef6e24cd0d6d6892b1970d93090c66bc
DerekWW <derek.wierson@gmail.com>
2018-01-13 16:56:39 -0800

    tidy up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
+router.use('/', require('./api/home'))
 router.use('/api/users', require('./api/users'))
 router.use('/api/auth', require('./api/auth'))
-router.use('/', require('./api/home'))
 
 module.exports = router
