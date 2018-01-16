0e16df77e4345eb8c3d3a8f40165707168a761ea
DerekWW <derek.wierson@gmail.com>
2018-01-15 21:38:22 -0800

    rename knexfile and add migration folder

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,11 @@ module.exports = {
       host: 'postgres://localhost/diary_analyzer',
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
-    }
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: './seeds'
   },
 
   production: {
