655a45001b4988b889010bb4f8af2fe5f1ea3ed0
DerekWW <derek.wierson@gmail.com>
2018-01-16 20:11:49 -0800

    finish dogs migration file

--- db/migrations/20180115215104_dogs.js
+++ db/migrations/20180115215104_dogs.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('dogs', (table) => {
+    table.increments()
+    table.string('name').notNullable()
+    table.string('breed').notNullable()
+    table.integer('age').notNullable()
+    table.integer('weight').notNullable()
+    table.string('temperament').notNullable()
+    table.integer('user_id').notNullable().references('id').inTable('users')
+  })
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('dogs')
+}
