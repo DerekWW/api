e83ae352f72eb02361646d22d33b191037223d06
DerekWW <derek.wierson@gmail.com>
2018-01-17 21:23:28 -0800

    add in first test model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const knex = require('../../knex')
 const router = express.Router()
+const User = require('../../db/schemas/user')
 
 router.get('/', (req, res, next) => {
   res.send('at users')
@@ -8,8 +8,20 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   console.log(req.body)
+  if (!req.body.name) {
+    next('No Name Supplied')
+  }
+  let newUser = new User({
+    name: req.body.name
+  })
 
-  res.send(req.body)
+  newUser.save((err, newUser) => {
+    if (err) {
+      return console.error(err)
+    }
+    console.log(`User ${newUser.name} saved to db`)
+    res.send(req.body)
+  })
 })
 
 module.exports = router
